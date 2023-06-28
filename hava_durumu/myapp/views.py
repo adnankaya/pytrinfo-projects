e5ff22c34e25a0e368f5fdd82f54458541773d69
adnankaya <adnankaya.py@gmail.com>
2023-06-28 04:11:42 +0300

    hava durumu project part 3 URL parameter all

--- hava_durumu/myapp/views.py
+++ hava_durumu/myapp/views.py
@@ -14,5 +14,21 @@ def index(request):
         else:
             new_weather = Weather.objects.create(**data_dict)
         return JsonResponse(data_dict)
+    # eger all parametresi client tarafindan gonderilmisse butun kayitlari cevap olarak döndür
+    all = request.GET.get("all")
+    if all:
+        weathers = Weather.objects.all()
+        serialized_data = [
+            {
+                "id": obj.id,
+                "city": obj.city,
+                "icon": obj.icon,
+                "temperature": obj.temperature,
+                "description": obj.description,
+                "updated_date": obj.updated_date.strftime('%Y-%m-%d %H:%M')
+            }
+            for obj in weathers
+        ]
+        return JsonResponse(serialized_data, safe=False)
 
     return render(request, "myapp/index.html")
