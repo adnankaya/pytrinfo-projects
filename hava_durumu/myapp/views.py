3f1ff4c34f9f945782f1e179f23736cd0262cee1
adnankaya <adnankaya.py@gmail.com>
2023-06-30 06:06:56 +0300

    hava durumu project part 5: delete city with websocket

--- hava_durumu/myapp/views.py
+++ hava_durumu/myapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http.response import JsonResponse # yeni
+from django.http.response import JsonResponse
 # internals
-from .utils import request_to_weatherapi # yeni
+from .utils import request_to_weatherapi
 from .models import Weather
-from .consumers import active_consumers # yeni
+from .consumers import active_consumers
 
 def index(request):
     city = request.GET.get("city")
@@ -33,9 +33,28 @@ def index(request):
         return JsonResponse(serialized_data, safe=False)
     # websocket ile haberleşme yapmak için view üzerinden tetikleme yapacagiz
     triggered = request.GET.get("trigger")
-    if triggered:
+    deleted_city = request.GET.get("deleted_city")
+    if triggered and deleted_city:
+        # silinecek objeyi bul
+        obj = Weather.objects.filter(city__iexact=deleted_city)
+        if obj.exists():
+            obj.delete()
+        # butun kayitlari getir
+        qset = Weather.objects.all()
+        # serialize ederek consumer'a gondermemiz gerekiyor.
+        qs_weathers = [
+            {
+                "id": obj.id,
+                "city": obj.city,
+                "icon": obj.icon,
+                "temperature": obj.temperature,
+                "description": obj.description,
+                "updated_date": obj.updated_date.strftime('%Y-%m-%d %H:%M')
+            }
+            for obj in qset
+        ]
         for consumer in active_consumers:
-            consumer.receive(qset={"data": "pytr.info sitesinden zengin içerikler..."})
+            consumer.receive(qset=qs_weathers)
         return JsonResponse({"message":"Tetiklendi!"}, safe=False)
 
     return render(request, "myapp/index.html")
